923f662d20690d7066b2f96a54460d2f4db38f3b
Yaiziuss <yaizaa23.04@gmail.com>
2019-03-17 11:17:33 +0100

    Add files via upload

--- Pruebas/PruebaCatalogoEtiquetas.java
+++ Pruebas/PruebaCatalogoEtiquetas.java
@@ -6,9 +6,7 @@ import Euskoflix.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,11 +19,12 @@ class PruebaCatalogoEtiquetas {
 	/**
 	 * @throws java.lang.Exception
 	 */
-private static CatalogoEtiquetas ce=CatalogoEtiquetas.getMiCEti();
+private static CatalogoEtiquetas ce;
     
     
-@BeforeEach
+	@BeforeEach
     public void setUp() {
+		ce=CatalogoEtiquetas.getMiCEti();
     }
     
     @AfterEach
@@ -42,9 +41,6 @@ private static CatalogoEtiquetas ce=CatalogoEtiquetas.getMiCEti();
      */
     public void PruebaAnnadirPeliAEtiqueta() {
             ce.annadirPeliAEtiqueta("Hp1");
-    //	assertEquals(p1.getPesoEtiqueta("Magia"), 4);
-    //	assertNotSame(p1.getPesoEtiqueta("Amistad"),6);
-    //	assertEquals(p1.getPesoEtiqueta("Amor"),0);
             assertEquals(ce.getApariciones("Hp1"),1);
             assertNotEquals(ce.getApariciones("Hp1"),7);
             assertEquals(ce.getApariciones("hola"),0);
--- Pruebas/PruebaCatalogoPeliculas.java
+++ Pruebas/PruebaCatalogoPeliculas.java
@@ -5,9 +5,11 @@ package Pruebas;
 import Euskoflix.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 /**
  * @author yaiza
@@ -21,7 +23,8 @@ class PruebaCatalogoPeliculas {
 	private static CatalogoPeliculas ct1;
 	//private static Pelicula p1, p2, p3, p4;
 	
-	static void setUpBeforeClass() throws Exception {
+	@BeforeEach
+	public void setUp() throws Exception{
 		ct1= CatalogoPeliculas.getMiCPeli();
 		/*p2= new Pelicula("Your Name");
 		p3= new Pelicula("La Bella y la Bestia 2017");*/
@@ -38,30 +41,31 @@ class PruebaCatalogoPeliculas {
 	 * Vamos a probar si añade la pelicula correctamente o no
 	 */
 	@Test
+	public void PruebaGetPelis() {
+            assertNull(ct1.getPelicula(55));
+            assertEquals(ct1.getPelicula(1).getNombre(),"Capitana Marvel");
+	}
+	/**
+	 * Vamos a probar si añade la pelicula correctamente o no
+	 */
+	@Test
 	public void PruebaAnnadirPeli() {
 		ct1.annadirPelicula(1,"Capitana Marvel");
 		assertNotNull(ct1);
 		assertSame(ct1.getTotalPelis(), 1);
 	}
 
-	/**
-	 * Vamos a probar si la lista de peliculas es vacia o no
-	 */
-	@Test
-	public void PruebaGetPelis() {
-		assertNull(ct1.getListaPeliculas());
-		assertSame(ct1.getTotalPelis(),0);
-		ct1.annadirPelicula(1,"Capitana Marvel");
-		assertNotNull(ct1.getListaPeliculas());
-	}
+	
 	
 	/**
 	 * Vamos a probar si añade correctamente los pares
 	 */
+        
 	@Test
 	public void PruebaPares() {
-		ct1.annadirPelicula(1,"Capitana Marvel");
-		assertSame(ct1.getParejitasFelices(),"1,Capitana Marvel");
+            assertEquals(ct1.getTotalPelis(),0);
+            ct1.annadirPelicula(1,"Capitana Marvel");
+            assertEquals(ct1.getParejitasFelices().get(0),"1 Capitana Marvel");
 	}
 	
 	/**
@@ -69,9 +73,9 @@ class PruebaCatalogoPeliculas {
 	 */
 	@Test
 	public void PruebaTotalPelis() {
-		assertEquals(ct1.getTotalPelis(), 0);
-		ct1.annadirPelicula(1,"Capitana Marvel");
-		assertSame(ct1.getTotalPelis(),1);
+            assertEquals(1, ct1.getTotalPelis() );
+            ct1.annadirPelicula(2,"Capitan America");
+            assertEquals(ct1.getTotalPelis(),2);
 	}
 
 	//TODO Falta getPelicula y annadirEtiqueta
