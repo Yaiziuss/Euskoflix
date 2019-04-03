9adc68c2a265b4a17d53fcc6b7c2307cecf9c97b
Yaiziuss <yaizaa23.04@gmail.com>
2019-04-03 15:39:53 +0200

    Operación ArrayList

--- Euskoflix/Operaciones.java
+++ Euskoflix/Operaciones.java
@@ -15,24 +15,22 @@ public class Operaciones {
 	
 	//Modificar para poder usar tanto para peliculas como usuario
 	
-	public float media(int pIdUsuario) {
+	public float media(ArrayList<Float> pLista) {
 		float m=0;
-		ArrayList<Float> lp=CatalogoUsuarios.getMiCU().obtenerPuntuaciones(pIdUsuario);
-		int numVal=lp.size();
+		int numVal=pLista.size();
 		for (int i=0; i<numVal;i++) {
-			m=m+lp.get(i);
+			m=m+pLista.get(i);
 		}
 		m=m/numVal;
 		return m;
 	}
 
-	public double desviacionTipica(int pIdUsuario) {
+	public double desviacionTipica(ArrayList<Float> pLista) {
 		double destip=0;
-		double m=media(pIdUsuario)*1.0;
-		ArrayList<Float> lp=CatalogoUsuarios.getMiCU().obtenerPuntuaciones(pIdUsuario);
-		int numVal=lp.size();
+		double m=media(pLista)*1.0;
+		int numVal=pLista.size();
 		for (int i=0; i<numVal;i++) {
-			destip= destip+Math.pow(m-lp.get(i), 2);
+			destip= destip+Math.pow(m-pLista.get(i), 2);
 		}
 		destip=destip/numVal;
 		return Math.sqrt(destip);
